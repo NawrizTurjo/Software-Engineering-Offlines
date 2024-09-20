5a53a1cbc71ae49f732c490aeabee1ad98e3d001
NawrizTurjo <nawriz_ahmed03@hotmail.com>
2024-09-20 16:16:22 +0600

    2

---
+++ Offline-1 (Creational Design Pattern)/code/task2/src/Client/Client.java
@@ -0,0 +1,52 @@
+package Client;
+
+import java.util.Scanner;
+
+import AbstractBaseClass.Operation;
+import AbstractFactory.AbstractFactory;
+import Producer.FactoryProducer;
+
+public class Client {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter customer type: ");
+        String factoryType = scanner.nextLine();
+        System.out.print("Enter operation type: ");
+        String opeartionType = scanner.nextLine();
+
+        System.out.print("Enter amount: ");
+        int amount = scanner.nextInt();
+        System.out.print("Enter years: ");
+        int years = scanner.nextInt();
+
+        AbstractFactory customerFactory = FactoryProducer.getFactory(factoryType);
+
+        Operation operation = customerFactory.getLoan(amount);
+        System.out.println(
+                opeartionType + " amount after " + years + " years: " + operation.getTotalCompoundInterest(years));
+
+        scanner.nextLine();
+
+        System.out.print("Enter customer type: ");
+        factoryType = scanner.nextLine();
+
+        customerFactory = FactoryProducer.getFactory(factoryType);
+
+        System.out.print("Enter operation type: ");
+        opeartionType = scanner.nextLine();
+
+        System.out.print("Enter amount: ");
+        amount = scanner.nextInt();
+
+        System.out.print("Enter years: ");
+        years = scanner.nextInt();
+
+        operation = customerFactory.getAccount(amount);
+        System.out.println(
+                opeartionType + " amount after " + years + " years: " + operation.getTotalCompoundInterest(years));
+
+        scanner.close();
+
+    }
+}
