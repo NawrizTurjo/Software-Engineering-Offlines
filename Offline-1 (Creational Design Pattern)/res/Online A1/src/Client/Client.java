7c16b9687efa8e78de89d2c7541ad3c06b706f1d
NawrizTurjo <nawriz_ahmed03@hotmail.com>
2024-09-22 22:35:39 +0600

    online practice added

--- Offline-1 (Creational Design Pattern)/res/Online A1/src/Client/Client.java
+++ Offline-1 (Creational Design Pattern)/res/Online A1/src/Client/Client.java
@@ -9,7 +9,6 @@ public class Client {
         Scanner sc = new Scanner(System.in);
         System.out.print("input a path:");
         String path = sc.nextLine();
-        FileFactory fileFactory = new FileFactory();
-        fileFactory.getFile(path).loadDocument(path);
+        FileFactory.getFile(path).loadDocument(path);
     }
 }
